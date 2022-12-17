5e4584965e8104d68a8d7b49e6b776f11223c3e4
Kirstin Lyon <93132914+KirstinCLyon@users.noreply.github.com>
2022-12-17 20:21:12 +0100

    Add files via upload
    Signed-off-by: Kirstin Lyon <93132914+KirstinCLyon@users.noreply.github.com>

--- Scripts/worldbank data.R
+++ Scripts/worldbank data.R
@@ -1,10 +1,16 @@
 library(tidyverse)
-library(wbstats)
+library(wbstats) #World bank database
+library(httr)# needed for API
+library(jsonlite) #needed to read content from WHO
+library(WHO)
 
+# WorldBank Datasets ----
 
-#Pull down worldbank data
+#Pull down worldbank data 
 
 all_indicators <- wbstats::wb_indicators()
+
+wbstats::
 
 
 all_sources <- all_indicators %>% 
@@ -28,3 +34,74 @@ write_csv(all_indicators, "All WorldBank Indicators.csv")
 fresh_cache <- wb_cache()
 fresh_indicators <- fresh_cache$indicators
 fresh_countries <- fresh_cache$countries
+
+?wb_cache
+?wb_data
+
+df <- get_data("WHOSIS_000001")
+
+country <- wb_countries()
+
+# WHO Datasets ----
+
+# https://www.dataquest.io/blog/r-api-tutorial/
+#https://www.who.int/data/gho/info/gho-odata-api
+#you want the status to be 200 - that means it has worked
+#Status codes: https://www.restapitutorial.com/httpstatuscodes.html
+#https://gateway.euro.who.int/en/api/specification/#nav-example-4
+#https://apps.who.int/gho/data/node.resources.api
+#https://apps.who.int/gho/athena/public_docs/examples.html
+#https://apps.who.int/gho/data/node.home
+#https://data.who.int/products/datadot
+#https://portal.who.int/triplebillions/PowerBIDashboards/ExploreIndicators
+
+res = GET("https://ghoapi.azureedge.net/api/Dimension")
+res
+
+rawToChar(res$content)
+data = fromJSON(rawToChar(res$content))
+data
+
+test = GET("https://ghoapi.azureedge.net/api")
+test
+
+test2 = GET("http://apps.who.int/gho/athena/api/GHO/WHOSIS_000001,WHOSIS_000015")
+test2
+data_test2 = fromJSON(rawToChar(test2$content))
+
+
+rawToChar(test$content)
+data_test = fromJSON(rawToChar(test$content))
+
+data_test
+
+class(data)
+
+
+list1 <- data
+
+
+df <- map_if(list1, is.data.frame, list) %>% as_tibble %>% unnest(cols = c(value))
+df # this is what we want!
+class(df)
+
+
+# move to a tibble
+
+
+
+who_indicators <- WHO::get_codes()
+
+
+countries <- GET("https://ghoapi.azureedge.net/api/DIMENSION/COUNTRY/DimensionValues")
+countries_list <- fromJSON(rawToChar(countries$content))
+
+countries_list
+
+
+TB <- GET("https://ghoapi.azureedge.net/api/Indicator?$filter=contains(IndicatorName,'Tuberculosis')")
+TB_LIST <- fromJSON(rawToChar(TB$content))
+TB_LIST
+
+
+
